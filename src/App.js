1fdf291869c79dded38933a9d3204a4985081adc
Pablo Paez <pablopaez2307@gmail.com>
2023-05-03 22:30:21 -0300

    +fondo cambiante, banderas e idiomas

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import axios from 'axios'
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
-  /* agregar cambio de lenguaje y sistema metrico*/
-
-
+  const [temperature, setTemperature] = useState('')
+  const [flag_code, setFlag_code] = useState('')
 
   const urlSP = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=sp&appid=437dbe377b74e79d549ecd458c3de726`
   const urlEN = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=en&appid=437dbe377b74e79d549ecd458c3de726`
-  
+  let source = `https://flagsapi.com/${flag_code}/flat/64.png`
+
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
         if (isChecked == false) {
             axios.get(urlSP).then((response) => {
                 setData(response.data)
-                console.log(response.data)
+                setTemperature(response.data.main.temp)
+                setFlag_code(response.data.sys.country)
               })
               setLocation('')
         }
         else{
             axios.get(urlEN).then((response) => {
                 setData(response.data)
-                console.log(response.data)
+                setTemperature(response.data.main.temp)
+                setFlag_code(response.data.sys.country)
               })
               setLocation('')
         }
@@ -38,9 +40,43 @@ function App() {
      console.log(isChecked)
   }; 
 
+  
+  /* ENG ver */
+  const dateBuilderENG = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  /* ESP ver */
+  const dateBuilderSP = (d) => {
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+    let dia = dias[d.getDay()];
+    let fecha = d.getDate();
+    let mes = meses[d.getMonth()];
+    let año = d.getFullYear();
+
+    return `${dia} ${fecha} ${mes} ${año}`
+  }
 
   return (
-    <div className="app">
+      <div className={(typeof location != "undefined") && data.main ?
+        ((temperature < 10 && temperature != "undefined") ? 
+        'app cold' 
+        : ((temperature > 20 && temperature != "undefined") ? 
+        'app warm' : 'app')) 
+        : 'app'}>
+
+
+
       <div className="search">
         <input
           value={location}
@@ -56,41 +92,61 @@ function App() {
                     {isChecked ? "Checked" : "Unchecked"}
                 </label>
             <h2 className='lang_name'>ENG</h2>
-        </div>
-
-        
+        </div> 
       </div>
 
 
+
       <div className="container">
-        
         <div className="top">
           <div>
-            <p className="location">{data.name} {data.main ? <p>{data.sys.country}</p> : null}</p>
+            <p className="location">{data.name} {data.main ? <p>{data.sys.country} <img src={source}></img> </p> : null}</p>
           </div>
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div>
-            {data.weather ? 
+            {/* {data.weather ? 
             <p className="description">{data.weather[0].description}</p> 
-            : null}
+            : null} */}
+            {isChecked ? 
+            <p className="description">{data.weather[0].description}</p>
+            : <p className="description">{data.weather[0].description}</p>}
+
+            <div className="date">
+                {isChecked ? 
+                <p className="description"> {dateBuilderENG(new Date())} </p> 
+                : <p className="description"> {dateBuilderSP(new Date())} </p>}
+            </div>
+            
           </div>
         </div>
 
         {data.name !== undefined &&
           <div className="bottom">
             <div className="feels">
+                
               {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
-              <p>Sensacion</p>
+              
+              {isChecked ? 
+              <p>Feels Like</p> 
+              : <p>Sensacion</p>}
+              
             </div>
             <div className="humidity">
               {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humedad</p>
+              
+              {isChecked ? 
+              <p>Humidity</p> 
+              : <p>Humedad</p>}
+
             </div>
             <div className="wind">
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Vientos</p>
+              
+              {isChecked ? 
+              <p>Winds</p> 
+              : <p>Vientos</p>}
               
             </div>
           </div>
